e4b0a9a8008d9d089906fea50a58b1502aeb3efa
JamsterMan <ben.brad.h@gmail.com>
2021-02-22 15:06:05 -0500

    fixed mesh cube
    stopped unity smoothing with extra vertices

--- MineCraftClone/Assets/Scripts/CubeGenerator.cs
+++ MineCraftClone/Assets/Scripts/CubeGenerator.cs
@@ -28,7 +28,7 @@ public class CubeGenerator : MonoBehaviour
     }
 
     //
-    void CreateCube()
+    /*void CreateCube()//unity smoothes vertices together
     {
         vertices = new Vector3[]
         {
@@ -64,6 +64,64 @@ public class CubeGenerator : MonoBehaviour
             //bottom
             0, 6, 7,
             0, 1, 6,
+
+        };
+    }*/
+
+    void CreateCube()
+    {
+        vertices = new Vector3[]
+        {   //repeated vertices needed to stop unity from smoothing the vertices
+            new Vector3 (0,0,0),//0
+            new Vector3 (1,0,0),//1
+            new Vector3 (1,1,0),//2
+            new Vector3 (0,1,0),//3
+            new Vector3 (0,1,1),//4
+            new Vector3 (1,1,1),//5
+            new Vector3 (1,0,1),//6
+            new Vector3 (0,0,1),//7
+
+            new Vector3 (0,0,0),//0+8=8
+            new Vector3 (1,0,0),//1+8=9
+            new Vector3 (1,1,0),//2+8=10
+            new Vector3 (0,1,0),//3+8=11
+            new Vector3 (0,1,1),//4+8=12
+            new Vector3 (1,1,1),//5+8=13
+            new Vector3 (1,0,1),//6+8=14
+            new Vector3 (0,0,1),//7+8=15
+            
+            new Vector3 (0,0,0),//0+16=16
+            new Vector3 (1,0,0),//1+16=17
+            new Vector3 (1,1,0),//2+16=18
+            new Vector3 (0,1,0),//3+16=19
+            new Vector3 (0,1,1),//4+16=20
+            new Vector3 (1,1,1),//5+16=21
+            new Vector3 (1,0,1),//6+16=22
+            new Vector3 (0,0,1),//7+16=23
+            
+        };
+
+        //top, left, front one order; bottom, right, back different order for normals
+        triangles = new int[]
+        {
+            //front
+            0, 2, 1,
+            0, 3, 2,
+            //top
+            10, 11, 4,
+            10, 4, 5,
+            //right
+            9, 18, 13,
+            9, 13, 6,
+            //left
+            8, 7, 12,
+            8, 12, 19,
+            //back
+            21, 20, 15,
+            21, 15, 14,
+            //bottom
+            16, 22, 23,
+            16, 17, 22,
 
         };
     }
