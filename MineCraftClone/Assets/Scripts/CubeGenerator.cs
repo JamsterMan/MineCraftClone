bad6d4eb896d824bba77f918c81088888c1e73bf
JamsterMan <ben.brad.h@gmail.com>
2021-02-22 23:07:30 -0500

    Update CubeGenerator.cs
    adjusted mesh cube to allow culling of sides later on

--- MineCraftClone/Assets/Scripts/CubeGenerator.cs
+++ MineCraftClone/Assets/Scripts/CubeGenerator.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class CubeGenerator : MonoBehaviour
 {
-    
+    private const int cubeSides = 6;
     Mesh mesh;
     
     Vector3[] vertices;
-    int[] triangles;
+    int[][] triangles;//double array for culling sides of cube not visable
+    int[] visableTriangles;
 
     // Start is called before the first frame update
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
+        visableTriangles = new int[36];
 
         CreateCube();
         UpdateMesh();
@@ -26,47 +28,6 @@ public class CubeGenerator : MonoBehaviour
     {
         
     }
-
-    //
-    /*void CreateCube()//unity smoothes vertices together
-    {
-        vertices = new Vector3[]
-        {
-            new Vector3 (0,0,0),//0
-            new Vector3 (1,0,0),//1
-            new Vector3 (1,1,0),//2
-            new Vector3 (0,1,0),//3
-            new Vector3 (0,1,1),//4
-            new Vector3 (1,1,1),//5
-            new Vector3 (1,0,1),//6
-            new Vector3 (0,0,1),//7
-            
-        };
-
-        //top, left, front one order; bottom, right, back different order for normals
-        triangles = new int[]
-        {
-            //front
-            0, 2, 1,
-            0, 3, 2,
-            //top
-            2, 3, 4,
-            2, 4, 5,
-            //right
-            1, 2, 5,
-            1, 5, 6,
-            //left
-            0, 7, 4,
-            0, 4, 3,
-            //back
-            5, 4, 7,
-            5, 7, 6,
-            //bottom
-            0, 6, 7,
-            0, 1, 6,
-
-        };
-    }*/
 
     void CreateCube()
     {
@@ -102,26 +63,20 @@ public class CubeGenerator : MonoBehaviour
         };
 
         //top, left, front one order; bottom, right, back different order for normals
-        triangles = new int[]
+        triangles = new int[][]
         {
             //front
-            0, 2, 1,
-            0, 3, 2,
+            new int[] { 0, 2, 1, 0, 3, 2},
             //top
-            10, 11, 4,
-            10, 4, 5,
+            new int[] { 10, 11, 4, 10, 4, 5 },
             //right
-            9, 18, 13,
-            9, 13, 6,
+            new int[] { 9, 18, 13, 9, 13, 6 },
             //left
-            8, 7, 12,
-            8, 12, 19,
+            new int[] { 8, 7, 12, 8, 12, 19 },
             //back
-            21, 20, 15,
-            21, 15, 14,
+            new int[] { 21, 20, 15, 21, 15, 14 },
             //bottom
-            16, 22, 23,
-            16, 17, 22,
+            new int[] { 16, 22, 23, 16, 17, 22 }
 
         };
     }
@@ -131,7 +86,13 @@ public class CubeGenerator : MonoBehaviour
         mesh.Clear();
 
         mesh.vertices = vertices;
-        mesh.triangles = triangles;
+        for (int i = 0; i < cubeSides; i++) {//front, top, right, left, back, bottom
+            for (int j = 0; j < cubeSides; j++) {//
+                //remove non-visable sides
+                visableTriangles[j+(i*6)] = triangles[i][j];
+            }
+        }
+        mesh.triangles = visableTriangles;
         mesh.RecalculateNormals();
     }
 }
