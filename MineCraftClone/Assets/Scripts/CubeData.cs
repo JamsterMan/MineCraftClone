530b87a62ad141c3bf97e4e8e274549c50d96f80
JamsterMan <ben.brad.h@gmail.com>
2021-02-24 00:48:41 -0500

    refactored mesh cube generation
    fixed to allow any number of cubes to be drawn

--- MineCraftClone/Assets/Scripts/CubeData.cs
+++ MineCraftClone/Assets/Scripts/CubeData.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeData : MonoBehaviour
+public static class CubeData 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static readonly Vector3[] vertices = new Vector3[]
+        {   //repeated vertices needed to stop unity from smoothing the vertices
+            new Vector3 (0,0,0),//0
+            new Vector3 (1,0,0),//1
+            new Vector3 (1,1,0),//2
+            new Vector3 (0,1,0),//3
+            new Vector3 (0,1,1),//4
+            new Vector3 (1,1,1),//5
+            new Vector3 (1,0,1),//6
+            new Vector3 (0,0,1),//7
+        };
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    public static readonly int[,] triangles = new int[,]
+        {
+            //front
+            { 0, 2, 1, 0, 3, 2},
+            //top
+            { 2, 3, 4, 2, 4, 5 },
+            //right
+            { 1, 2, 5, 1, 5, 6 },
+            //left
+            { 0, 7, 4, 0, 4, 3 },
+            //back
+            { 5, 4, 7, 5, 7, 6 },
+            //bottom
+            { 0, 6, 7, 0, 1, 6 }
+
+        };
+
 }
--- MineCraftClone/Assets/Scripts/CubeGenerator.cs
+++ MineCraftClone/Assets/Scripts/CubeGenerator.cs
@@ -5,99 +5,51 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class CubeGenerator : MonoBehaviour
 {
+    //change class name to chuck generator
     private const int cubeSides = 6;
     Mesh mesh;
     
-    Vector3[] vertices;
-    int[][] triangles;//double array for culling sides of cube not visable
-    int[] visableTriangles;
+    //Vector3[] vertices;
+    //int[][] triangles;//double array for culling sides of cube not visable
+    List<int> visableTriangles;
+    List<Vector3> visableVertices;
+    int verticesIndex;
 
     // Start is called before the first frame update
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        visableTriangles = new int[36];
 
-        mesh.Clear();
-        CreateCube(0, 0, 0);
-        UpdateMesh();
-        CreateCube(0, 2, 0);
-        UpdateMesh();
-        CreateCube(0, 0, -1);
+        verticesIndex = 0;
+        visableTriangles = new List<int>();
+        visableVertices = new List<Vector3>();
+        CreateCube(new Vector3(0, 0, 0) );
+        CreateCube(new Vector3(0, 2, 0) );
+        CreateCube(new Vector3(-4, 0, 0) );
         UpdateMesh();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateCube(Vector3 cubePosition)
     {
-        
-    }
-
-    void CreateCube(int x, int y, int z)
-    {
-        vertices = new Vector3[]
-        {   //repeated vertices needed to stop unity from smoothing the vertices
-            new Vector3 (x,y,z),//0
-            new Vector3 (x+1,y,z),//1
-            new Vector3 (x+1,y+1,z),//2
-            new Vector3 (x,y+1,z),//3
-            new Vector3 (x,y+1,z+1),//4
-            new Vector3 (x+1,y+1,z+1),//5
-            new Vector3 (x+1,y,z+1),//6
-            new Vector3 (x,y,z+1),//7
-
-            new Vector3 (x,y,z),//0+8=8
-            new Vector3 (x+1,y,z),//1+8=9
-            new Vector3 (x+1,y+1,z),//2+8=10
-            new Vector3 (x,y+1,z),//3+8=11
-            new Vector3 (x,y+1,z+1),//4+8=12
-            new Vector3 (x+1,y+1,z+1),//5+8=13
-            new Vector3 (x+1,y,z+1),//6+8=14
-            new Vector3 (x,y,z+1),//7+8=15
-
-            new Vector3 (x,y,z),//0+16=16
-            new Vector3 (x+1,y,z),//1+16=17
-            new Vector3 (x+1,y+1,z),//2+16=18
-            new Vector3 (x,y+1,z),//3+16=19
-            new Vector3 (x,y+1,z+1),//4+16=20
-            new Vector3 (x+1,y+1,z+1),//5+16=21
-            new Vector3 (x+1,y,z+1),//6+16=22
-            new Vector3 (x,y,z+1),//7+16=23
-            
-        };
-
-        //top, left, front one order; bottom, right, back different order for normals
-        triangles = new int[][]
-        {
-            //front
-            new int[] { 0, 2, 1, 0, 3, 2},
-            //top
-            new int[] { 10, 11, 4, 10, 4, 5 },
-            //right
-            new int[] { 9, 18, 13, 9, 13, 6 },
-            //left
-            new int[] { 8, 7, 12, 8, 12, 19 },
-            //back
-            new int[] { 21, 20, 15, 21, 15, 14 },
-            //bottom
-            new int[] { 16, 22, 23, 16, 17, 22 }
-
-        };
+        int triangleIndex;
+        for (int i = 0; i < cubeSides; i++) {//front, top, right, left, back, bottom
+            for (int j = 0; j < cubeSides; j++) {
+                //remove non-visable sides
+                triangleIndex = CubeData.triangles[i, j];
+                visableVertices.Add(cubePosition + CubeData.vertices[triangleIndex]);
+                visableTriangles.Add(verticesIndex);
+                verticesIndex++;
+            }
+        }
     }
 
     void UpdateMesh()
     {
-        //mesh.Clear();
+        mesh.Clear();
 
-        mesh.vertices = vertices;
-        for (int i = 0; i < cubeSides; i++) {//front, top, right, left, back, bottom
-            for (int j = 0; j < cubeSides; j++) {//
-                //remove non-visable sides
-                visableTriangles[j+(i*6)] = triangles[i][j];
-            }
-        }
-        mesh.triangles = visableTriangles;
+        mesh.vertices = visableVertices.ToArray();
+        mesh.triangles = visableTriangles.ToArray();
         mesh.RecalculateNormals();
     }
 }
