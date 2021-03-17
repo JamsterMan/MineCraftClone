de60dcd0e8bb29d50f156dfbf71243fbbcf4e499
JamsterMan <ben.brad.h@gmail.com>
2021-03-16 22:21:23 -0400

    added deactivating chunks
    - deactivate chunks when the player gets too far away

--- MineCraftClone/Assets/Scripts/WorldGenerator.cs
+++ MineCraftClone/Assets/Scripts/WorldGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WorldGenerator : MonoBehaviour
 {
     public GameObject chunk;
-    public int WorldSize = 2;
+    public int WorldSize = 2;//number of chunks in active in the world
     public float perlinOffsetX, perlinOffsetZ;
     public float noiseScale = 0.3f, offsetScale = 10.0f;//perlin noise in unity changes base on the decimals, offsetScale determines how much the noise changes the height
 
@@ -15,16 +15,21 @@ public class WorldGenerator : MonoBehaviour
     public CubeData[] CubeTypes;
     private readonly int chunkSize = 10;//number of blocks in a chunk
     private int start, end;
+    private Vector2 lastPlayerPosition;
 
-    private Dictionary<Vector2, GameObject> worldMap;//used to keep track of chunks
+    private Dictionary<Vector2, GameObject> worldMap = new Dictionary<Vector2, GameObject>();//used to keep track of chunks
+    private List<Vector2> activeChunks = new List<Vector2>();
 
 
     // Start is called before the first frame update
     void Start()
     {
-        worldMap = new Dictionary<Vector2, GameObject>();
+        //worldMap = new Dictionary<Vector2, GameObject>();
+        //activeChunks = new List<GameObject>();
         perlinOffsetX = Random.Range(0f, 9999f);//for random world geration
         perlinOffsetZ = Random.Range(0f, 9999f);//for random world geration
+        lastPlayerPosition.x = player.position.x;
+        lastPlayerPosition.y = player.position.z;
 
         start = (int)Mathf.Floor(WorldSize / 2f);
         end = (int)Mathf.Ceil(WorldSize / 2f);
@@ -39,8 +44,13 @@ public class WorldGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LoadVisableChunks();
-        UnloadChunks();
+        if (lastPlayerPosition.x != player.position.x || lastPlayerPosition.y != player.position.z) {//check if the player hased moved
+            lastPlayerPosition.x = player.position.x;
+            lastPlayerPosition.y = player.position.z;
+            LoadVisableChunks();
+            UnloadChunks();
+        }
+
     }
 
     void LoadVisableChunks()//create or load in chunks
@@ -61,13 +71,20 @@ public class WorldGenerator : MonoBehaviour
 
     void UnloadChunks()
     {
-
+        foreach (Vector2 activeChunkKey in activeChunks) {//check if the active chunks are too far away from the player
+            if(Mathf.Abs(activeChunkKey.x - (player.position.x / chunkSize)) > WorldSize/2 || Mathf.Abs(activeChunkKey.y - (player.position.z / chunkSize)) > WorldSize/2) {//player.postion / chunksize = chunk pos
+                worldMap[activeChunkKey].SetActive(false);
+            }
+        }
     }
 
     void CreateChunk(Vector2 key, int x, int z)
     {
         Vector3 pos = new Vector3(x * chunkSize, 0, z * chunkSize);
-        worldMap.Add(key, Instantiate(chunk, pos, Quaternion.identity));//chunks # will be player position/10 rounded down
+        GameObject curChunk = Instantiate(chunk, pos, Quaternion.identity);
+        curChunk.transform.parent = transform;
+        worldMap.Add(key, curChunk);//chunks # will be player position/10 rounded down
+        activeChunks.Add(key);
     }
 }
 
