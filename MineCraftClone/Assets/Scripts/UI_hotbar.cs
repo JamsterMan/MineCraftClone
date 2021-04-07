0d87bc090da86bab5e38648ec9f211fcfb164b23
JamsterMan <ben.brad.h@gmail.com>
2021-04-07 15:52:02 -0400

    simple hotbar added
    -needs improved block icons
    -still choses block from block list instead of what the hot bar says

--- MineCraftClone/Assets/Scripts/UI_hotbar.cs
+++ MineCraftClone/Assets/Scripts/UI_hotbar.cs
@@ -7,10 +7,15 @@ public class UI_hotbar : MonoBehaviour
 {
     public Text tempHotbar;
     public WorldGenerator world;
+    public RectTransform hotbarHighlight;
+    public Vector3 highlightStartPos;
 
     private int itemIndex;
     private string text;
     private CubeData.CubeType type;
+
+    private float hotbarItemSize = 55.7894f;
+    private int hotbarSize = 9;
 
     void Start()
     {
@@ -18,18 +23,28 @@ public class UI_hotbar : MonoBehaviour
         text = "Current Block: ";
         type = world.CubeTypes[itemIndex].getCubeType();
         tempHotbar.text = text + type.ToString();
+        highlightStartPos = hotbarHighlight.localPosition;
+        hotbarItemSize = hotbarHighlight.sizeDelta.x;
     }
 
     void Update()
     {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if( scroll > 0) {//scroll up
+        if (scroll < 0) {//scroll up
             IndexIncrement(1);
-            type = world.CubeTypes[itemIndex].getCubeType();
+            if (itemIndex <= world.CubeTypes.Length - 1) {
+                type = world.CubeTypes[itemIndex].getCubeType();
+            } else {
+                type = world.CubeTypes[0].getCubeType();//air
+            }
             tempHotbar.text = text + type.ToString();
-        } else if (scroll < 0){//scroll down
+        } else if (scroll > 0){//scroll down
             IndexIncrement(-1);
-            type = world.CubeTypes[itemIndex].getCubeType();
+            if (itemIndex <= world.CubeTypes.Length - 1) {
+                type = world.CubeTypes[itemIndex].getCubeType();
+            } else {
+                type = world.CubeTypes[0].getCubeType();//air
+            }
             tempHotbar.text = text + type.ToString();
         }
     }
@@ -37,9 +52,12 @@ public class UI_hotbar : MonoBehaviour
     void IndexIncrement(int incerment)
     {
         itemIndex += incerment;
+        hotbarHighlight.localPosition += new Vector3(incerment * hotbarItemSize,0,0);
         if(itemIndex < 1) {
-            itemIndex = world.CubeTypes.Length -1;
-        }else if (itemIndex > world.CubeTypes.Length - 1) {
+            hotbarHighlight.localPosition = highlightStartPos + new Vector3((hotbarSize-1) * hotbarItemSize, 0, 0);
+            itemIndex = hotbarSize;
+        }else if (itemIndex > hotbarSize) {
+            hotbarHighlight.localPosition = highlightStartPos;
             itemIndex = 1;
         }
     }
